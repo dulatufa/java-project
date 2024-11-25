794b89fdc36b22e846897efd88745714a1f6bcff
dulatufa <dulatufa@gmail.com>
2024-11-25 16:36:28 -0600

    Updated src folder files with comments for assignment clarity

--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -1,26 +1,27 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+// Unit tests for the Rectangle class
 public class RectangleTest {
-  Rectangle myRectangle = new Rectangle(5,6);
+  Rectangle myRectangle = new Rectangle(5, 6);
 
   @Test
   public void testGetArea() {
-    assertEquals(myRectangle.getArea(), 30);
+    assertEquals(myRectangle.getArea(), 30); // Test for area
   }
 
   @Test
   public void testGetPerimeter() {
-    assertEquals(myRectangle.getPerimeter(), 22);
+    assertEquals(myRectangle.getPerimeter(), 22); // Test for perimeter
   }
 
   @Test
   public void testLength() {
-    assertEquals(myRectangle.length, 5);
+    assertEquals(myRectangle.length, 5); // Test for length
   }
 
   @Test
   public void testWidth() {
-    assertEquals(myRectangle.width, 6);
+    assertEquals(myRectangle.width, 6); // Test 
   }
 }
